a08166bc72df47d9b0d1f234e6e953a675469a28
FIRCALiviu <liviu.firca@gmail.com>
2023-03-13 20:11:06 +0200

    added generator

--- generator.py
+++ generator.py
@@ -1,6 +1,13 @@
-import numpy.random as np
-test=open("test_set.in","w")
-array=np.randint(0,10**7,2*10**7)
-for integer in array:
-    test.write("{} ".format(integer))
-test.close()
+import numpy as np
+tests=[ open("test_set1.in","w") , open("test_set2.in","w")   ,open("test_set3.in","w")   ,open("test_set4.in","w"), open("test_set5.in","w"), open("test_set6.in","w"), open("test_set7.in","w"), open("test_set8.in","w"), open("test_set9.in","w")]
+test_size=[10**3,10**6,10**8]
+sizes=[10**3,10**6,10**8]
+test_num=0
+for array_size in test_size:
+    for size in sizes:
+        data=np.random.randint(0,size,array_size)
+        for num in data:
+            tests[test_num].write(hex(num)+' ') # outputing the values in hex so it uses less space
+        tests[test_num].close()
+        test_num+=1
+        
---
+++ test.py
@@ -0,0 +1,3 @@
+num=13928349798103
+for i in range(1000000):
+    x="{:x} ".format(num)
