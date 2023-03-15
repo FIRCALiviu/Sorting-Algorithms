4dc57ebfd69239002ec5ce46f75899c6ed72cd7f
FIRCALiviu <liviu.firca@gmail.com>
2023-03-15 23:11:18 +0200

    /bin/python

--- gen_plots.py
+++ gen_plots.py
@@ -2,30 +2,44 @@ import matplotlib.pyplot as plt
 data=open("results.txt","r")
 algorithms=data.read().split("./")
 del algorithms[0]
-fig, plots=plt.subplots(nrows=3,ncols=1)
-sizes=[10**3,10**6,10**8]
 
-for i,size in enumerate(sizes):
-   
-    for algorithm in algorithms:
-        
-        lines=algorithm.splitlines()
-        name=lines[0]
-        del lines[0]
-        dataX=[]
-        dataY=[]
-        for line in lines:
-            words=line.split()
-            maxsize=int(words[0].split('=')[1])
-            array_size=int(words[1].split("=")[1])
-            if words[2]=='timeout':
-                time=float(500) # we cap the data at 500
-            else:
-                time=float(words[2])
-            if maxsize==size:
-                dataX.append(array_size)
-                dataY.append(time)
-            else:
-                continue
-        
+size=10**8
+colors=[
+'b' ,
+'g' ,
+'r' ,
+'c' ,
+'m' ,
+'y' ,
+'k' ,
 
+'tab:orange',
+'tab:pink'
+]
+
+for j,algorithm in enumerate(algorithms):
+    
+    lines=algorithm.splitlines()
+    name=lines[0]
+    del lines[0]
+    dataX=[]
+    dataY=[]
+    for line in lines:
+        words=line.split()
+        maxsize=int(words[0].split('=')[1])
+        array_size=int(words[1].split("=")[1])
+        if words[2]=='timeout':
+            time=float(500) # we cap the data at 500
+        else:
+            time=float(words[2])
+        if maxsize==size:
+            dataX.append(array_size)
+            dataY.append(time)
+        else:
+            continue
+    plt.plot(dataX,dataY,color=colors[j],label=name,linewidth=2)
+plt.title("max size={}".format(size))    
+plt.legend()
+plt.yscale('log') 
+plt.xscale('log')       
+plt.show()
